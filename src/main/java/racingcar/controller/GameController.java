77e26d8b31d59d689de971304bf911de247d7ae5
mainfn <_@mainfn.dev>
2023-09-02 00:33:47 +0900

    feat: GameController 모든 기능 추가 완료

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,9 +1,7 @@
 package racingcar.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import racingcar.domain.Car;
 import racingcar.domain.Cars;
+import racingcar.service.GameService;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -11,25 +9,29 @@ public final class GameController {
 
   private final OutputView outputView;
   private final InputView inputView;
+  private final GameService gameService;
 
   public GameController(
       final OutputView outputView,
-      final InputView inputView
+      final InputView inputView,
+      final GameService gameService
   ) {
     this.outputView = outputView;
     this.inputView = inputView;
+    this.gameService = gameService;
   }
 
-  // controller/GameController.java
   public void start() {
     // 1. Cars를 받아옴
     final Cars cars = inputView.inputCars();
 
-    // 3. 진행할 라운드 받아옴
+    // 2. 진행할 라운드 받아옴
     final int maxRound = inputView.inputMaxRound();
 
-    // 4. 게임 시작
+    // 3. 게임 시작
+    gameService.play(cars, maxRound);
 
-    // 5. 결과 출력
+    // 4. 결과 출력
+    outputView.printGameResult(cars.into());
   }
 }
