80904e8b4a53b52c2be0bc88916814f8d7c02571
mainfn <_@mainfn.dev>
2023-09-06 01:16:27 +0900

    empty

--- src/main/java/racingcar/exception/InvalidRoundsToPlayException.java
+++
@@ -1,8 +0,0 @@
-package racingcar.exception;
-
-public class InvalidRoundsToPlayException extends IllegalArgumentException {
-
-  public InvalidRoundsToPlayException() {
-    super("시도 횟수는 숫자여야 한다.");
-  }
-}
