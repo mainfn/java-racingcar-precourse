e60d10bbe48572144770777626d0431bf50cb423
mainfn <_@mainfn.dev>
2023-09-29 17:00:56 +0900

    feat: RandomNumberGenerator 난수 생성 기능

---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,21 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public final class RandomNumberGenerator {
+
+  private final int startInclusive;
+  private final int endInclusive;
+
+  public RandomNumberGenerator(
+      final int startInclusive,
+      final int endInclusive
+  ) {
+    this.startInclusive = startInclusive;
+    this.endInclusive = endInclusive;
+  }
+
+  public int generate() {
+    return Randoms.pickNumberInRange(startInclusive, endInclusive);
+  }
+}
